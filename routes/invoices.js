2c754dbe03c2c557b6f189e46bf70f8efdc0608a
Edgar Sanchez-Barreda <edgarsanchez@Edgars-MBP.attlocal.net>
2022-07-20 15:51:11 -0700

    started part 3

--- routes/invoices.js
+++ routes/invoices.js
@@ -3,5 +3,14 @@ const ExpressError = require('../expressError');
 const router = express.Router();
 const db = require('../db');
 
+// Routes
+router.get('/', async (req, res, next) => {
+    try {
+        const results = await db.query();
+    } catch (err) {
+        return next(err);
+    }
+});
+
 // Router
 module.exports = router;
