4bec47ad5615af05cc44e5788ff203c8cbae6374
Edgar Sanchez <edgar@Edgars-MacBook-Air.local>
2022-07-20 15:17:59 -0700

    Added first invoices route

--- routes/invoices.js
+++ routes/invoices.js
@@ -3,5 +3,16 @@ const ExpressError = require('../expressError');
 const router = express.Router();
 const db = require('../db');
 
+// Routes
+
+router.get('/', async (req, res, next) => {
+	try {
+		const results = await db.query(`SELECT id, comp_code FROM invoices`);
+		return res.json({ invoices: results.rows });
+	} catch (err) {
+		return next(err);
+	}
+});
+
 // Router
 module.exports = router;
